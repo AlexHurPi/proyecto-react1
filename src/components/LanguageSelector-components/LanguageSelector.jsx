c37fde8ff40dadc4027eb62689c4e98baf82ebd4
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2026-03-03 21:19:44 -0500

    commit15

--- src/components/LanguageSelector-components/LanguageSelector.jsx
+++ src/components/LanguageSelector-components/LanguageSelector.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import './LanguageSelector.css'
+import './languageSelectorStyles.css'
 
 const LanguageSelector = () => {
   const { i18n } = useTranslation();
